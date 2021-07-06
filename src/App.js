d167c7a7237a49b38e2ae96898059c19d72eae9b
gracenak <gracenak@gmail.com>
2021-07-06 14:34:32 -0700

    create components folder with footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -7,17 +9,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Portfolio Frontend.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        > */}
+          {/* See Grace's Software Portfolio
+        </a> */}
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div classname= "footer">
+            © 2021 Grace Yuiko Nakano
+        </div>
+    )
+}
+
+export default Footer
