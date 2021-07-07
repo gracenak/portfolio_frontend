f4ce94c785554bdd28e963d77c43bee86e495d87
gracenak <gracenak@gmail.com>
2021-07-06 19:56:06 -0700

    add navbar link components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
 import './App.css';
 import React from 'react';
 import Footer from './components/Footer'
 import Header from './components/Header'
 import NavBar from './components/NavBar'
+import About from './components/About'
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path='/about' component={About} />
+        <Header />
+      {/* <header className="App-header">
       <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to Portfolio Frontend.
         </p>
-        {/* <a
+        <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        > */}
-          {/* See Grace's Software Portfolio
-        </a> */}
-      </header>
-      <Footer />
-    </div>
+        >
+          See Grace's Software Portfolio
+        </a>
+      </header> */}
+      </Switch>
+      <Footer />  
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div>
+            <p>
+                I am a Fullstack Software Engineer with a professional background as a classical violinist, 
+                serving as the Assistant Concertmaster of the Tucson Symphony Orchestra.
+            </p>
+            <p>
+                A recent graduate from the Flatiron School, I am passionate about integrating my skills and
+                interests into software development. 
+            </p>
+            <p>
+                Technical skills include React, Ruby on Rails, JavaScript ES6+, HTML & CSS, Sinatra
+                and usage of PostgreSQL and SQLite databases.
+            </p>
+            <p>
+                My aspiration to helps others and impact positive change; socially, economically, environmentally,
+                led me towards the career path as a software enginner. I want to help build creative solutions to 
+                problems on a larger scale through web development. 
+            </p>
+            <p>
+                Other passions include travel, food, hiking, yoga, and running. I am currently training
+                for my first marathon!(🙈 🤩) I love connecting with others, sharing ideas and collaborating
+                on projects.
+            </p>
+            <p>
+                "If you were born with 
+                the weakness to fail
+                you were born 
+                the strength to rise"
+                - Rupi Kaur
+            </p>
+
+
+
+            
+        </div>
+    )
+}
+
+export default About
