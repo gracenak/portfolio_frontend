1d9a6fa56334914d14e9f03645a3e9d69c7b19f5
gracenak <gracenak@gmail.com>
2021-07-09 22:39:56 -0700

    delete NavBar component and fix alignment for NavLinks

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from 'react';
 import SideBar from './components/SideBar'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import NavBar from './components/NavBar'
 import About from './components/About'
 import Contact from './components/Contact'
 import Projects from './components/Projects'
@@ -22,7 +21,7 @@ function App() {
         <Route exact path='/projects' component={Projects} />
         <Route exact path='/blogs' component={Blogs} />
         
-        <NavBar />
+       
         <Header />
         
  
