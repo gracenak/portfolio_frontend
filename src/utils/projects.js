a7a19563f5fec2d1b3ab2c9792d6e42b48a5465d
gracenakano <Grace.Nakano@launchpointfitness.io>
2021-07-20 23:01:58 -0700

    add folder to store data in variable in frontend

---
+++ src/utils/projects.js
@@ -0,0 +1,54 @@
+export const bonVivant = [
+    {title: "Bon Vivant",
+    summary: "A collaborative recipe application that allows users to create, share, edit and review recipes",
+    description: [
+        'Developed MVP SPA React/Redux Frontend application with a Rails API backend that demonstrates client-server communication using SQLite database & serializers',
+        'Implemented Redux/Thunk middleware to respond, modify state change & make async actions to send & receive data from server',
+        'Styled frontend with custom CSS' 
+    ],
+    link: "https://github.com/gracenak/bon_vivant_frontend.git"}
+]
+
+export const visualize = [
+    {title: "Visualize",
+    summary: "A collaborative vision board application that allows users to share their aspirations and motivations ",
+    description: [
+        "Built MVP SPA using JavaScript frontend and Rails API backend, demonstrating client server communication",
+        "Employed PostgreSQL & serializers",  
+        "Integrated ES6+, Object-oriented JavaScript classes & functions to encapsulate related data and behavior",
+        "Utilized Bootstrap & custom CSS to style frontend" 
+    ],
+    link: "https://github.com/gracenak/visualize_frontend.git"}
+]
+
+export const freelanceApp = [
+    {title: "Freelance Application",
+    summary: "Job search and posting resource application for freelancers equipped with job application requests",
+    description: [
+        "Developed full CRUD Ruby on Rails app that manages related data through complex forms and RESTful routes",
+        "Utilized ActiveRecord/SQL to query the database with SQLite and implemented third-party authentication via omniAuth gems, validations, & flash messages",
+        "Implemented helper methods & partials to protect the views of the current user and limit views & logic in controller"
+    ],
+    link: "https://github.com/gracenak/freelance_app.git"}
+]
+
+export const freelanceExpense = [
+    {title: "Freelance Expense Tracker",
+    summary: "Expense tool that can be utilized by freelancers to keep track of gigs and expenses",
+    description: [
+        "Built an Object Oriented Ruby Application using ActiveRecord with Sinatra framework and SqlQLite database",
+        "Implemented full CRUD, MVP, incorporating sign up/login/logout features",
+        "Demonstrated RESTful route conventions and usage of validations and flash messages"
+    ],
+    link: "https://github.com/gracenak/freelance_expense_tracker.git"}
+]
+
+export const concertCli = [
+    {title: "Concert CLI",
+    summary: "A CLI application that displays a list of of concerts and their details upon selection",
+    description: [
+        "CLI application to an external data source via scraping",
+        "Demonstrates ability to write concise Object Oriented Ruby code"
+    ],
+    link: "https://github.com/gracenak/gnakano_cli_project.git"}
+]
