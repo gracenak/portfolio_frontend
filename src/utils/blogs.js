64636532b5e1a4a41dd075988a6779f51bcf7674
Grace Nakano <gracenak@gmail.com>
2021-07-26 12:22:00 -0500

    create utils/blogs file to save blog objects to render to frontend

---
+++ src/utils/blogs.js
@@ -0,0 +1,5 @@
+export const blog1 = [
+    {title:"Jingle Bell Rails: Associations and Nested Forms All the Way",
+    link: "https://gracenak.medium.com/jingle-bell-rails-associations-and-nested-forms-all-the-way-31ce77e4e4f8"
+    }
+]
