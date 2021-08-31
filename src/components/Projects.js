ec686739a99e1fc95235a769e5d734494a31490f
Grace Nakano <gracenak@gmail.com>
2021-08-30 22:51:20 -0700

    fix div class errors and import project objects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,68 +1,83 @@
-import React from 'react';
+import React from "react";
 // import { bonVivant } from '../utils/projects'
-import Project from './Project';
-import '../styles/Projects.css'
-import projects from '../utils/projects'
+import Project from "./Project";
+import "../styles/Projects.css";
+import {
+  bonVivant,
+  visualize,
+  freelanceApp,
+  freelanceExpense,
+  concertCli,
+} from "../utils/projects";
 
-const Projects = ({projects}) => {
-    console.log({projects})
-    const card = projects && projects.map(project => 
-        <Project key={project.title}
-                 attributes={project}
-        />)
+const Projects = () => {
+  return (
+    // <div className="projects-container">
+    //      <div className="projects-scroller">
+    //          <div className="image-container">
+    //          <img src="https://i.pinimg.com/originals/3e/7a/08/3e7a08b130a6eb6ef1c923fbe13dbdf5.jpg" width = "300" height = "300" alt='Home'/>
+    //          <div className="content-container">
+    //          <div className="text-title">
+    //             <p>Bon Vivant</p>
+    //             <p>Visualize</p>
+    //          </div>
 
+    //              {card}
+    //              <h2>Projects</h2>
 
+    //          </div>
+    //      </div>
+    //      </div>
+    //  </div>
+    <div className="w3-container">
+      <h2>Projects</h2>
 
-    return(
-        
-        // <div className="projects-container">
-        //      <div className="projects-scroller">
-        //          <div className="image-container">
-        //          <img src="https://i.pinimg.com/originals/3e/7a/08/3e7a08b130a6eb6ef1c923fbe13dbdf5.jpg" width = "300" height = "300" alt='Home'/>
-        //          <div className="content-container">
-        //          <div className="text-title">
-        //             <p>Bon Vivant</p>
-        //             <p>Visualize</p>
-        //          </div>
+      <div className="w3-card-4">
+        <img
+          src="https://i.pinimg.com/originals/3e/7a/08/3e7a08b130a6eb6ef1c923fbe13dbdf5.jpg"
+          alt="bon-vivant"
+          width="300"
+          height="300"
+        />
+        <div className="w3-container w3-center">
+          <h2>{bonVivant && bonVivant.title}</h2>
+          <h4>{bonVivant.description}</h4>
+          <a href="https://github.com/gracenak/bon_vivant_frontend.git">
+            {bonVivant && bonVivant.title}
+          </a>
+        </div>
+      </div>
 
-            
-        //              {card}
-        //              <h2>Projects</h2>
-
-                   
-        //          </div>
-        //      </div>
-        //      </div>
-        //  </div>
-        <div class="w3-container">
-  <h2>Projects</h2>
-
-  <div class="w3-card-4" >
-    <img src="https://i.pinimg.com/originals/3e/7a/08/3e7a08b130a6eb6ef1c923fbe13dbdf5.jpg" alt="bon-vivant" width="300" height="300"  />
-    <div class="w3-container w3-center">
-      <p>Bon Vivant</p>
-      <h4>A collaborative recipe application that allows users to create, share, edit and review recipes</h4>
-      <a>https://github.com/gracenak/bon_vivant_frontend.git</a>
+      <div className="w3-card-4">
+        <img
+          src="https://media.giphy.com/media/tP7n5MgP1y5dC/giphy.gif"
+          alt="visualize"
+          width="300"
+          height="300"
+        />
+        <div className="w3-container w3-center">
+          <p>Visualize</p>
+          <h4>
+            A collaborative vision board application that allows users to share
+            their aspirations and motivations{" "}
+          </h4>
+          <a href="https://github.com/gracenak/visualize_frontend.git">
+            Visualize
+          </a>
+          <li>
+            Built MVP SPA using JavaScript frontend and Rails API backend,
+            demonstrating client server communication
+          </li>
+          <li>Employed PostgreSQL & serializers</li>
+          <li>
+            Integrated ES6+, Object-oriented JavaScript classes & functions to
+            encapsulate related data and behavior
+          </li>
+          <li>Utilized Bootstrap & custom CSS to style frontend</li>
+        </div>
+      </div>
     </div>
-</div>
+  );
+};
 
-    <div class="w3-card-4" >
-    <img src="https://media.giphy.com/media/tP7n5MgP1y5dC/giphy.gif" alt="visualize" width="300" height="300"  />
-    <div class="w3-container w3-center">
-      <p>Visualize</p>
-      <h4>A collaborative vision board application that allows users to share their aspirations and motivations </h4>
-      <a>https://github.com/gracenak/visualize_frontend.git</a>
-      <li>Built MVP SPA using JavaScript frontend and Rails API backend, demonstrating client server communication</li>
-      <li>Employed PostgreSQL & serializers</li>  
-      <li>Integrated ES6+, Object-oriented JavaScript classes & functions to encapsulate related data and behavior</li>
-      <li>Utilized Bootstrap & custom CSS to style frontend</li>
-    </div>
-
-  
-</div>
-</div>
-    )
-
-}
-
-export default Projects
+export default Projects;
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,37 +1,46 @@
-import { Component } from 'react'
-import { connect } from 'react-redux'
-import { Route, Switch } from 'react-router-dom'
-import { fetchProjects } from '../actions/fetchProjects'
-import About from '../components/About'
-import Contact from '../components/Contact'
-import Projects from '../components/Projects'
-import Blogs from '../components/Blogs'
-import Project from '../components/Project'
+import { Component } from "react";
+import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import { fetchProjects } from "../actions/fetchProjects";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Projects from "../components/Projects";
+import Blogs from "../components/Blogs";
+import Project from "../components/Project";
 
 class PortfolioContainer extends Component {
-    render() {
-        return(
-            <div>
-            <Switch>
-                <Route exact path='/about' component={About} />
-                <Route exact path='/contact' component={Contact} />
-                <Route exact path='/projects/:id' render={(routerProps) => <Project {...routerProps} projects={this.props.projects && this.props.projects}/>} />
-                <Route exact path='/projects' render={(routerProps) => <Projects {...routerProps} projects={this.props.projects}  />} />
-                <Route exact path='/blogs' component={Blogs} />
-            </Switch> 
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route
+            exact
+            path="/projects/:id"
+            render={(routerProps) => (
+              <Project
+                {...routerProps}
+                projects={this.props.projects && this.props.projects}
+              />
+            )}
+          />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/blogs" component={Blogs} />
+        </Switch>
+      </div>
+    );
+  }
 
-    componentDidMount(){
-        this.props.fetchProjects()
-    }
+  componentDidMount() {
+    this.props.fetchProjects();
+  }
 }
 
-    const mapStateToProps = state => {
-    return {
-        recipes: state.projects
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    recipes: state.projects,
+  };
+};
 
-export default connect(mapStateToProps, { fetchProjects })(PortfolioContainer)
+export default connect(mapStateToProps, { fetchProjects })(PortfolioContainer);
