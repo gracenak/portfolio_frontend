aa724eaa7383d69a172dc321ce2f3b0fa8634d56
Grace Nakano <gracenak@gmail.com>
2021-08-30 22:25:03 -0700

    import blog object from utils

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
+import { blog1 } from "../utils/blogs";
 
 const Blogs = () => {
-    return(
-        <div>
-            Blogs
-        </div>
+  return (
+    <div>
+      <h1>Blogs</h1>
+      <h2>{blog1.title}</h2>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Blogs
+export default Blogs;
