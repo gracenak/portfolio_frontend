377048adae77c431a95d2ae074fbba143137adff
Grace Nakano <gracenak@gmail.com>
2021-09-06 22:52:12 -0700

    add another blog object for render

--- src/utils/blogs.js
+++ src/utils/blogs.js
@@ -2,3 +2,8 @@ export const blog1 = {
   title: "Jingle Bell Rails: Associations and Nested Forms All the Way",
   link: "https://gracenak.medium.com/jingle-bell-rails-associations-and-nested-forms-all-the-way-31ce77e4e4f8",
 };
+
+export const blog2 = {
+  title: "Understandu=ing the DOM and the Three Pillars",
+  link: "https://gracenak.medium.com/understanding-dom-and-the-three-pillars-df6d7675c662",
+};
